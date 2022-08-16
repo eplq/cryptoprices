e7089a075ab4ebbd16645b9b9c0b7aa8de166eeb
eplq <91148468+eplq@users.noreply.github.com>
2022-08-16 22:50:24 +0200

    revert commit

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -8,6 +8,4 @@ test('testing api', async () => {
     expect(coins[0].id).toBe("bitcoin")
     expect(coins[1].id).toBe("ethereum")
     expect(coins[2].id).toBe("tether")
-    
-    expect(false).toBeTruthy()
 })
