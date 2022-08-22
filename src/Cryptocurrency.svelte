cd79b4a3ff6d18191d2d367deb0a04508ed5dcd8
eplq <91148468+eplq@users.noreply.github.com>
2022-08-23 00:44:16 +0200

    coin detail

--- src/Cryptocurrency.svelte
+++ src/Cryptocurrency.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import Accordion, { Panel, Header, Content } from '@smui-extra/accordion'
-    import type { Coin } from './types'
+    import Accordion, { Panel, Header, Content } from "@smui-extra/accordion";
+    import type { Coin } from "./types";
 
-    export let coin: Coin
+    export let coin: Coin;
 </script>
 
 <Accordion>
@@ -14,7 +14,34 @@
             </div>
         </Header>
         <Content>
-            {coin.current_price}
+            <table class="info-table">
+                <tr>
+                    <th>24h</th>
+                    <td
+                        class:red={coin.price_change_percentage_24h < 0}
+                        class:green={coin.price_change_percentage_24h > 0}
+                        >{coin.price_change_percentage_24h.toFixed(2)} % ({coin.price_change_24h.toFixed(2)} &euro;)
+                    </td>
+                </tr>
+                <tr>
+                    <th>Circulating supply</th>
+                    <td>
+                        {coin.circulating_supply} {coin.symbol.toUpperCase()}
+                    </td>
+                </tr>
+                <tr>
+                    <th>Max supply</th>
+                    <td>
+                        {coin.max_supply ?? "Infinite"} {coin.symbol.toUpperCase()}
+                    </td>
+                </tr>
+                <tr>
+                    <th>Market Cap</th>
+                    <td>
+                        {coin.market_cap.toLocaleString("es-ES")}
+                    </td>
+                </tr>
+            </table>
         </Content>
     </Panel>
 </Accordion>
@@ -26,10 +53,26 @@
     }
 
     .header-div h2 {
-        font-size: max(min(1.5rem, 2vmax), .8rem);
+        font-size: max(min(1.5rem, 2vmax), 1rem);
         max-width: 50%;
-        
+
         overflow-x: hidden;
         text-overflow: ellipsis;
+    }
+
+    .info-table {
+        width: 100%;
+    }
+
+    .info-table td {
+        font-size: max(min(2vmin, 1rem), .8rem);
+    }
+
+    .red {
+        color: red;
+    }
+
+    .green {
+        color: green;
     }
 </style>
