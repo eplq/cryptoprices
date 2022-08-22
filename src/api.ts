e3a5e0197472badd6d6d07e70257c41faa4ab782
eplq <91148468+eplq@users.noreply.github.com>
2022-08-23 00:13:13 +0200

    api 200 per page

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import type { Coin } from './types'
 
 export async function getCoins() {
-    let request = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d')
+    let request = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=200&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d')
     let data: Array<Coin> = await request.json()
     return data;
 }
