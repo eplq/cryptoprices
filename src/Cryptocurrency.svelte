289e507e5e1996c09c6ef82cc5c96eb9070749d0
eplq <91148468+eplq@users.noreply.github.com>
2022-08-23 00:13:04 +0200

    accordion header name and price

--- src/Cryptocurrency.svelte
+++ src/Cryptocurrency.svelte
@@ -7,11 +7,29 @@
 
 <Accordion>
     <Panel>
-        <Header>
-            <h2>{coin.name}</h2>
+        <Header style="padding: 0;">
+            <div class="header-div">
+                <h2>{coin.name}</h2>
+                <span>{coin.current_price} &euro;</span>
+            </div>
         </Header>
         <Content>
             {coin.current_price}
         </Content>
     </Panel>
 </Accordion>
+
+<style>
+    .header-div {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .header-div h2 {
+        font-size: max(min(1.5rem, 2vmax), .8rem);
+        max-width: 50%;
+        
+        overflow-x: hidden;
+        text-overflow: ellipsis;
+    }
+</style>
